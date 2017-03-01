b7427f6a28f5ec3c383ff130ae317b053fad647d
Matt Watson <mattdangerw@gmail.com>
2017-02-28 22:30:09 -0800

    index: implement dbus interface

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { app, BrowserWindow } from 'electron'
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer'
-
+import { registerService } from 'dbus'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -39,14 +39,50 @@ const createWindow = async () => {
   })
 }
 
+let service
+
+const setupDBus = () => {
+  service = registerService('session', 'com.endlessm.electron.myths.en')
+  const object = service.createObject('/com/endlessm/electron/myths/en')
+  const iface = object.createInterface('com.endlessm.KnowledgeSearch')
+  iface.addMethod('LoadItem', {
+    in: [
+      { type: 's', name: 'EknID' },
+      { type: 's', name: 'Query' },
+      { type: 'u', name: 'Timestamp' }
+    ]
+  }, (id, query, timestamp, callback) => {
+    mainWindow.webContents.send('LoadItem', id, query, timestamp)
+    callback()
+  })
+  iface.addMethod('LoadQuery', {
+    in: [
+      { type: 's', name: 'Query' },
+      { type: 'u', name: 'Timestamp' }
+    ]
+  }, (query, timestamp, callback) => {
+    mainWindow.webContents.send('LoadQuery', query, timestamp)
+    callback()
+  })
+  iface.update()
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  // Wait till the window has finished loading before registering on the bus, so
+  // we can send messages straight over to the browser process.
+  mainWindow.webContents.on('did-finish-load', () => {
+    setupDBus()
+  })
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   app.quit()
+  if (service) service.disconnect()
 })
 
 app.on('activate', () => {
